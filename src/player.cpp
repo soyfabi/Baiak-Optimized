ac2dba25c481133740a1280726c0086c7cfcb8e3
Fabian Marzan <fabi.marzan1762@yahoo.com>
2024-08-08 21:19:31 -0400

    Added Boost and Reflect System

--- src/player.cpp
+++ src/player.cpp
@@ -1841,7 +1841,7 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 {
 	BlockType_t blockType = Creature::blockHit(attacker, combatType, damage, checkDefense, checkArmor, field, ignoreResistances);
 
-	if (attacker) {
+	if (attacker && combatType != COMBAT_HEALING) {
 		sendCreatureSquare(attacker, SQ_COLOR_BLACK);
 	}
 
@@ -1854,7 +1854,10 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 		return BLOCK_ARMOR;
 	}
 
-	for (int32_t slot = CONST_SLOT_FIRST; slot <= CONST_SLOT_LAST; ++slot) {
+	Reflect reflect;
+
+		size_t combatIndex = combatTypeToIndex(combatType);
+		for (int32_t slot = CONST_SLOT_FIRST; slot <= CONST_SLOT_LAST; ++slot) {
 		if (!isItemAbilityEnabled(static_cast<slots_t>(slot))) {
 			continue;
 		}
@@ -1874,7 +1877,7 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 			continue;
 		}
 
-		const int16_t& absorbPercent = it.abilities->absorbPercent[combatTypeToIndex(combatType)];
+		const int16_t& absorbPercent = it.abilities->absorbPercent[combatIndex];
 		if (absorbPercent != 0) {
 			damage -= std::round(damage * (absorbPercent / 100.));
 
@@ -1884,8 +1887,10 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 			}
 		}
 
+		reflect += item->getReflect(combatType);
+
 		if (field) {
-			const int16_t& fieldAbsorbPercent = it.abilities->fieldAbsorbPercent[combatTypeToIndex(combatType)];
+			const int16_t& fieldAbsorbPercent = it.abilities->fieldAbsorbPercent[combatIndex];
 			if (fieldAbsorbPercent != 0) {
 				damage -= std::round(damage * (fieldAbsorbPercent / 100.));
 
@@ -1894,6 +1899,14 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 					g_game.transformItem(item, item->getID(), charges - 1);
 				}
 			}
+		}
+		
+		if (attacker && reflect.chance > 0 && reflect.percent != 0 && uniform_random(1, 100) <= reflect.chance) {
+			CombatDamage reflectDamage;
+			reflectDamage.primary.type = combatType;
+			reflectDamage.primary.value = -std::round(damage * (reflect.percent / 100.));
+			reflectDamage.origin = ORIGIN_REFLECT;
+			g_game.combatChangeHealth(this, attacker, reflectDamage);
 		}
 	}
 
