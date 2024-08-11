7d4af32d2185168fdcfa0473a89d9a21bfcc6b36
Fabian Marzan <fabi.marzan1762@yahoo.com>
2024-08-10 20:12:54 -0400

    remove msg.addByte(lightInfo.level); of protocolgame.cpp

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -2097,7 +2097,6 @@ void ProtocolGame::AddCreature(NetworkMessage& msg, const Creature* creature, bo
 
 	LightInfo lightInfo = creature->getCreatureLight();
 	msg.addByte(player->isAccessPlayer() ? 0xFF : lightInfo.level);
-	msg.addByte(lightInfo.level);
 	msg.addByte(lightInfo.color);
 
 	msg.add<uint16_t>(creature->getStepSpeed());
@@ -2204,7 +2203,6 @@ void ProtocolGame::AddWorldLight(NetworkMessage& msg, LightInfo lightInfo)
 {
 	msg.addByte(0x82);
 	msg.addByte(player->isAccessPlayer() ? 0xFF : lightInfo.level);
-	msg.addByte(lightInfo.level);
 	msg.addByte(lightInfo.color);
 }
 
@@ -2215,7 +2213,6 @@ void ProtocolGame::AddCreatureLight(NetworkMessage& msg, const Creature* creatur
 	msg.addByte(0x8D);
 	msg.add<uint32_t>(creature->getID());
 	msg.addByte(player->isAccessPlayer() ? 0xFF : lightInfo.level);
-	msg.addByte(lightInfo.level);
 	msg.addByte(lightInfo.color);
 }
 
